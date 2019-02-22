d018220eaa4664308d9b22c3f6d2e01c1679b014
Erik Gustafson <erik.d.gustafson@gmail.com>
2019-02-22 10:36:10 -0600

    Rebrand
    Add unicorn image. The image, along with 'unicorn' header and 'sign up'
    button form the cutest 'lil unicorn head. Sign up button is the nose.
    
    Remove tagline about disrupting the world. We're still disrupting the
    world, but market research has shown better brand engagement among users
    when optimized for cuteness.
    
    Move 'sign in' button to top right corner. A/B testing has shown that
    this has nearly quadrupled our convesion rates! Existing users can no
    longer find the 'sign in' button - instead they just create a new
    account. Our numbers have never been better!
    
    We've now raised a Series B round of $55M. We're opening new offices in
    New York, Singapore and Amsterdam. We're hiring new teams of rockstar
    developers and product managers.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,39 +1,86 @@
 import React from 'react'
+import Unicorn from '../unicorn.png'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import { Link } from 'react-router-dom'
 
 function LandingPage() {
    return (
-      <div>
-         <Typography component="h1" variant="h1" align="center" gutterBottom style={{marginTop: 48}}>
-            Unicorn
-         </Typography>
-         <Typography variant="subtitle1" align="center" gutterBottom >
-            disrupting the world with revolutionary technology
-         </Typography>
+      <>
          <div
             style={{
-               display: 'flex',
-               marginTop: 16 
+               backgroundImage: `url(${Unicorn})`,
+               backgroundPosition: '50% 50%',
+               backgroundSize: 'contain',
+               backgroundRepeat: 'no-repeat',
+               margin: '0 auto',
+               maxWidth: 800,
+               minHeight: 400,
+               marginBottom: (
+                  // >= 500  -> 16px
+                  // 400-500 -> 0px
+                  // 340-400 -> -24px
+                  // < 340   -> -60px
+                  window.innerWidth >= 500 ? 16
+                     : (window.innerWidth < 500 && window.innerWidth > 400) ? 0
+                        : (window.innerWidth < 340) ? -60
+                           : -24
+               )
             }}
-         >
+         />
+         <div>
+            <Typography
+               component="h3" variant="h3"
+               align="center"
+               gutterBottom
+               style={{
+                  color: '#494939',
+                  fontSize: (window.innerWidth < 520 ? '8vw' : ''),
+               }}
+            > unicorn
+            </Typography>
             <div
                style={{
                   display: 'flex',
-                  flexDirection: 'column',
-                  margin: '0 auto'
+                  marginTop: 16
                }}
             >
-               <Button variant="contained" color="primary" component={Link} to="/signup" style={{margin: '8px 0px'}}>
-                  Sign Up
-               </Button>
-               <Button variant="outlined" color="primary" component={Link} to="/signin" >
-                  Sign In
-               </Button>
+               <div
+                  style={{
+                     display: 'flex',
+                     flexDirection: 'column',
+                     margin: '0 auto'
+                  }}
+               >
+                  <Button
+                     variant="contained"
+                     component={Link} to="/signup"
+                     color="primary"
+                     size="small"
+                     style={{
+                        margin: '8px 0px',
+                        // backgroundColor: '#00e300',
+                        // backgroundColor: '#eb5490',
+                        color: 'white'
+                     }}
+                  > Sign Up
+                  </Button>
+                  <Button
+                     component={Link} to="/signin"
+                     color="secondary"
+                     size="small"
+                     style={{
+                        position: 'absolute',
+                        top: 16,
+                        right: 16
+                        // color: '#eb5490'
+                     }}
+                  > Sign In
+                  </Button>
+               </div>
             </div>
          </div>
-      </div>
+      </>
    )
 }
 
