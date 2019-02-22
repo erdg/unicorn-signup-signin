a7c1b0617096143213305fe4ef82f927510ca488
Erik Gustafson <erik.d.gustafson@gmail.com>
2019-02-21 21:31:56 -0600

    Refactor class components -> fn components w/hooks

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,47 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import EntryForm from './EntryForm'
 import Typography from '@material-ui/core/Typography'
 import EmailInput from './EmailInput'
 import PasswordInput from './PasswordInput'
 import Button from '@material-ui/core/Button'
 
-class Signin extends React.Component {
+function Signin() {
 
-   state = {
-      email: '',
-      passw: ''
-   }
+   const [email, setEmail] = useState('')
+   const [passw, setPassw] = useState('')
 
-   render() {
-      return (
-         <EntryForm>
-            <Typography component="h4" variant="h4" gutterBottom >
-               Sign In
-            </Typography>
-            <EmailInput
-               id="signin-email"
-               name="email"
-               value={this.state.email}
-               onChange={this.onChange}
-               style={{marginBottom: 8}}
-            />
-            <PasswordInput
-               id="signin-passw"
-               name="passw"
-               value={this.state.passw}
-               onChange={this.onChange}
-               style={{marginBottom: 8}}
-            />
-            <Button variant="contained" color="primary" type="submit" >
-               Sign In
-            </Button>
-         </EntryForm>
-      )
-   }
+   return (
+      <EntryForm>
+         <Typography component="h4" variant="h4" gutterBottom >
+            Sign In
+         </Typography>
+         <EmailInput
+            id="signin-email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{marginBottom: 8}}
+         />
+         <PasswordInput
+            id="signin-passw"
+            name="passw"
+            value={passw}
+            onChange={(e) => setPassw(e.target.value)}
+            style={{marginBottom: 8}}
+         />
+         <Button variant="contained" color="primary" type="submit" >
+            Sign In
+         </Button>
+      </EntryForm>
+   )
 
-   onChange = (e) => {
-      this.setState({ [e.target.name]: e.target.value })
-   }
 }
 
 export default Signin
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,47 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import EntryForm from './EntryForm'
 import Typography from '@material-ui/core/Typography'
 import EmailInput from './EmailInput'
 import PasswordInput from './PasswordInput'
 import Button from '@material-ui/core/Button'
 
-class Signup extends React.Component {
+function Signup() {
 
-   state = {
-      email: '',
-      passw: ''
-   }
+   const [email, setEmail] = useState('')
+   const [passw, setPassw] = useState('')
 
-   render() {
-      return (
-         <EntryForm>
-            <Typography component="h4" variant="h4" gutterBottom >
-               Sign Up
-            </Typography>
-            <EmailInput
-               id="signup-email"
-               name="email"
-               value={this.state.email}
-               onChange={this.onChange}
-               style={{marginBottom: 8}}
-            />
-            <PasswordInput
-               id="signup-passw"
-               name="passw"
-               value={this.state.passw}
-               onChange={this.onChange}
-               style={{marginBottom: 8}}
-            />
-            <Button variant="contained" color="primary" type="submit" >
-               Sign Up
-            </Button>
-         </EntryForm>
-      )
-   }
+   return (
+      <EntryForm>
+         <Typography component="h4" variant="h4" gutterBottom >
+            Sign Up
+         </Typography>
+         <EmailInput
+            id="signup-email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{marginBottom: 8}}
+         />
+         <PasswordInput
+            id="signup-passw"
+            name="passw"
+            value={passw}
+            onChange={(e) => setPassw(e.target.value)}
+            style={{marginBottom: 8}}
+         />
+         <Button variant="contained" color="primary" type="submit" >
+            Sign Up
+         </Button>
+      </EntryForm>
+   )
 
-   onChange = (e) => {
-      this.setState({ [e.target.name]: e.target.value })
-   }
 }
 
 export default Signup
