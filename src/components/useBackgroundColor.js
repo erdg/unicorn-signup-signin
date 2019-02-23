37f80ccecfc8cc064b7543f917930cd965fc99ec
Erik Gustafson <erik.d.gustafson@gmail.com>
2019-02-22 18:04:37 -0600

    Remove gratuitous comments

--- src/components/useBackgroundColor.js
+++ src/components/useBackgroundColor.js
@@ -1,8 +1,5 @@
-// useBackgroundColor.js
 import { useEffect } from 'react'
 
-// sets document.body.style.backgroundColor to color on mount
-// sets ... to white on unmount
 function useBackgroundColor(color) {
    useEffect(() => {
       document.body.style.backgroundColor = color
