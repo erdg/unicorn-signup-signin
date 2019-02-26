459c2da2d2a0cb58c0c3efd250870a574a7a5da4
Erik Gustafson <erik.d.gustafson@gmail.com>
2019-02-25 21:48:39 -0600

    Add routing/navigation tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { render, fireEvent, cleanup } from 'react-testing-library'
+import App, { AppHistory } from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App Routing', () => {
+   const wrapper = render(<App />)
+   it('renders the landing page', () => {
+      expect(wrapper.getByText('unicorn')).toBeDefined()
+   })
+   it('renders the signup page', () => {
+      // click 'sign up' buton
+      fireEvent.click(wrapper.getByText(/sign up/i))
+      expect(wrapper.getByText('Sign Up')).toBeDefined()
+   })
+   it('renders the signin page', () => {
+      // route back to landing page
+      AppHistory.push('/')
+      // click 'sign in' button
+      fireEvent.click(wrapper.getByText(/sign in/i))
+      expect(wrapper.getByText('Sign In')).toBeDefined()
+   })
+   afterAll(() => {
+      cleanup(wrapper)
+   })
+})
+
